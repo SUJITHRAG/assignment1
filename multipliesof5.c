b894e6258b80c070775a61f1d2f00d8e632e84e3
SUJITHRAG <34641575+SUJITHRAG@users.noreply.github.com>
2018-03-07 19:43:16 +0530

    Create multipliesof5.c

---
+++ multipliesof5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+        int sum=0,i,n;
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("%d\n",5*i);
+        }
+        return 0;
+}
