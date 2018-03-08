5bf423af4d7adb0a47a93a91421c308497295459
SUJITHRAG <34641575+SUJITHRAG@users.noreply.github.com>
2018-03-08 09:26:40 +0530

    Create sumofnum.c

---
+++ sumofnum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int a[100],i,sum=0,n,k;
+	scanf("%d%d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<k;i++)
+	{
+		sum=sum+a[i];
+	}
+	printf("%d",sum);
+	return 0;
+}
