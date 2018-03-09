1b038e8410a3c87af9d72b48d6379535bbd8c0df
SUJITHRAG <34641575+SUJITHRAG@users.noreply.github.com>
2018-03-09 07:53:30 +0530

    Create prgm6.c

---
+++ Set 8/prgm6.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+    int n,i,t=0;
+
+
+
+    scanf("%d",&n);
+
+
+
+    for(i=2; i<=n/2; ++i)
+
+    {
+
+       
+
+        if(n%i==0)
+
+        {
+
+            t++;
+
+            break;
+
+        }
+
+    }
+
+
+
+    if (t==0)
+
+        printf("no");
+
+    else
+
+        printf("yes");
+
+    
+
+    return 0;
+
+}
